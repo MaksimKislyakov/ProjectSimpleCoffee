d87200609ac022127e08fe2f3b7e164eb74a1607
MaksimKislyakov <maksimkislakov35@gmail.com>
2025-11-25 18:23:37 +0500

    refactor(report_services): перенос логики калькулятора в отдельный файл

---
+++ backend/services/calculate_service.py
@@ -0,0 +1,31 @@
+from typing import List, Tuple
+from datetime import datetime
+
+
+class CalculateServices:
+    def _count_work_time(dates: List[List[datetime]]) -> Tuple[int, int]:
+        total_seconds = 0
+        
+        for start_time, end_time, _ in dates:
+            if start_time and end_time and end_time > start_time:
+                duration = end_time - start_time
+                total_seconds += duration.total_seconds()
+        
+        total_minutes = int(total_seconds // 60)
+        hours = total_minutes // 60
+        minutes = total_minutes % 60
+        
+        return hours, minutes
+    
+    def _count_shifts_day(dates: List[List[datetime]]) -> int:
+        work_days = 0
+
+        for _, _, status in dates:
+            if status == "Рабочая смена":
+                work_days += 1
+
+        return work_days
+    
+    def _count_total_salary(hourly_rate: int, total_hours: int) -> float:
+        total_salary = hourly_rate * total_hours
+        return total_salary
--- backend/services/report_service.py
+++ backend/services/report_service.py
@@ -14,6 +14,8 @@ from schemas.schedule_schemas import ScheduleCreate
 
 from models.roleEnum import RolesEnum
 
+from services.calculate_service import CalculateServices
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -36,38 +38,10 @@ class ReportService:
             for schedule in my_schedules
         ]
         
-        total_hours, total_minutes = self._count_work_time(dates)
-        work_days = self._count_shifts_day(dates)
-        total_salary = self._count_total_salary(current_user.hourly_rate, total_hours)
+        total_hours, total_minutes = CalculateServices._count_work_time(dates)
+        work_days = CalculateServices._count_shifts_day(dates)
+        total_salary = CalculateServices._count_total_salary(current_user.hourly_rate, total_hours)
 
         return {"work_hours": total_hours, 
                 "work_days": work_days,
                 "total_earnings": total_salary}
-
-    def _count_work_time(self, dates: List[List[datetime]]) -> Tuple[int, int]:
-        total_seconds = 0
-        
-        for start_time, end_time, _ in dates:
-            if start_time and end_time and end_time > start_time:
-                duration = end_time - start_time
-                total_seconds += duration.total_seconds()
-        
-        total_minutes = int(total_seconds // 60)
-        hours = total_minutes // 60
-        minutes = total_minutes % 60
-        
-        return hours, minutes
-    
-    def _count_shifts_day(self, dates: List[List[datetime]]) -> int:
-        work_days = 0
-
-        for _, _, status in dates:
-            if status == "Рабочая смена":
-                work_days += 1
-
-        return work_days
-    
-    def _count_total_salary(self, hourly_rate: int, total_hours: int) -> float:
-        total_salary = hourly_rate * total_hours
-        return total_salary
-        
