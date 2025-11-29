c6ca7b43d252c9a8a1e2f12ca35fa873894c442a
MaksimKislyakov <maksimkislakov35@gmail.com>
2025-11-29 15:18:03 +0500

    refactor(user_model.py): обновить модель юзера

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -32,7 +32,7 @@ class User(Base):
     patronymic = Column(String, nullable=True)
     email = Column(String, unique=True, index=True, nullable=False)
     telephone = Column(String, unique=True, index=True, nullable=False)
-    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=True)
     coffee_shop_id = Column(Integer, ForeignKey("coffeeshops.id"), nullable=True)
     hourly_rate = Column(Numeric(precision=6, scale=2), nullable=True)
     assessment_rate = Column(Integer, nullable=True, default=0)
